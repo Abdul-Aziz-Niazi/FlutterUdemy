21ba8b45283f5daed21f6affbaa7c14a35092d19
Abdul-Aziz-Niazi <niazi.abdulaziz@gmail.com>
2020-11-19 12:57:15 +0500

    I AM Rich App

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,4 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(home: Text('Hello World'),));
+void main() => runApp(MyApp());
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text('I Am Poor'),
+        ),
+        body: Center(
+            child: Image(
+          image: NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+        )),
+      ),
+    );
+  }
+}
